fae769d9a5f3030729359945c16591f8cf5ddfc8
KushagraSingh9 <102950718+KushagraSingh9@users.noreply.github.com>
2024-01-19 17:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+        function showDetails() {
+            // Get form values
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var email = document.getElementById("email").value;
+
+            // Create alert message
+            var message = "User Details:\n\n";
+            message += "First Name: " + firstName + "\n";
+            message += "Last Name: " + lastName + "\n";
+            message += "Phone Number: " + phoneNumber + "\n";
+            message += "Email: " + email;
+
+            // Display alert
+            alert(message);
+        }
